4b488b7d020bd09724a02224e3c3a05b74929c63
Prabhat <prabhat@Prabhats-MacBook-Air.local>
2018-11-20 22:11:01 +0530

    Initial Commit

---
+++ ProtocolDelegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProtocolDelegate
+//
+//  Created by Prabhat on 20/11/18.
+//  Copyright © 2018 Prabhat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
